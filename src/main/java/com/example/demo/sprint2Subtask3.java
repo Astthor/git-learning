af4955e1901e3610efe5530d6d5d5c49c346883c
Astthor <a.bragason@gmail.com>
2020-11-24 18:25:39 +0100

    Committing the changes made

--- src/main/java/com/example/demo/sprint2Subtask3.java
+++ src/main/java/com/example/demo/sprint2Subtask3.java
@@ -2,5 +2,5 @@ package com.example.demo;
 
 public class sprint2Subtask3 {
 
-    //Sprint2Subtask3
+    //Changing comment for conflict inside subtask6 which has it's own upstream. Sprint2Subtask3
 }
