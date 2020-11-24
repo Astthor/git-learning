59d9389a0a961281379eee0c566e5d098d8efa06
Astthor <a.bragason@gmail.com>
2020-11-24 18:30:21 +0100

    Testing -a -m commit, did a few lines of comments inside subtask6 for some conflict testing

--- src/main/java/com/example/demo/sprint2Subtask3.java
+++ src/main/java/com/example/demo/sprint2Subtask3.java
@@ -3,4 +3,8 @@ package com.example.demo;
 public class sprint2Subtask3 {
 
     //Changing comment for conflict inside subtask6 which has it's own upstream. Sprint2Subtask3
+    // Comment in subtask6
+    // Another comment
+        // Testing a bunch of comments to try and create some conflicts
+    
 }
