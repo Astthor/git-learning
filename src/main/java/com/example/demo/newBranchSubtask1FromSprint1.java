23b29cbec3ab812991e591408b71b5024793c226
Astthor <a.bragason@gmail.com>
2020-11-24 15:53:46 +0100

    added newBranchSubtask1FromSprint1.java while on branch subtask1

---
+++ src/main/java/com/example/demo/newBranchSubtask1FromSprint1.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class newBranchSubtask1FromSprint1 {
+
+    /*
+    * while on sprint1, created branch "subtask1" checked out to it, and added this .java
+    *
+    * */
+}
