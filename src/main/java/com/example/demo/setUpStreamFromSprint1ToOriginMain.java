418070de7d01f1c51769eb47aea2b94a9986c0e5
Astthor <a.bragason@gmail.com>
2020-11-24 16:43:16 +0100

    Changed comments in setUpStream...Java file to create conflict in other branchse.

--- src/main/java/com/example/demo/setUpStreamFromSprint1ToOriginMain.java
+++ src/main/java/com/example/demo/setUpStreamFromSprint1ToOriginMain.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 public class setUpStreamFromSprint1ToOriginMain {
 
+    // Creating conflict inside subtask2 - > Should give conflict everywhere. 
     // I created this class after having checkd out to sprint1 and set the upstream to origin/master with this command:
     // git branch -u origin/main
 }
