3fdd919fb7581b556b547c7e442bccd6c16e4234
Astthor <a.bragason@gmail.com>
2020-11-24 18:32:00 +0100

    Comments for conflict in subtask7

--- src/main/java/com/example/demo/sprint2Subtask3.java
+++ src/main/java/com/example/demo/sprint2Subtask3.java
@@ -3,4 +3,7 @@ package com.example.demo;
 public class sprint2Subtask3 {
 
     //Changing comment for conflict inside subtask6 which has it's own upstream. Sprint2Subtask3
+    // Comment inside Subtask7
+        //Subtask7
+    //Subtask7
 }
